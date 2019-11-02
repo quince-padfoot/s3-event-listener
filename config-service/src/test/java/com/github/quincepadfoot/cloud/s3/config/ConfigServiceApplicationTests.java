79090e68592b96c057ed8be953e00430fed8118a
Quince Padfoot <quince.padfoot@gmail.com>
2019-11-02 12:16:56 -0400

    #1 Added config-service module

---
+++ config-service/src/test/java/com/github/quincepadfoot/cloud/s3/config/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.quincepadfoot.cloud.s3.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
